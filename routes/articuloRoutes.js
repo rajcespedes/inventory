222e0574769ff5013f4a5243f67b7aa32fb3fe21
Raudo Cespedes <raj.cespedes@gmail.com>
2020-07-05 04:12:17 -0400

    Handling query returns for nuevoArticulo file

--- routes/articuloRoutes.js
+++ routes/articuloRoutes.js
@@ -3,7 +3,9 @@ const 	express 		= require('express'),
 		mongoose		= require('mongoose'),
 		methodOverride	= require('method-override'),
 		bodyParser		= require('body-parser'),
-		Articulo 		= require('../models/articulo');
+		Articulo 		= require('../models/articulo'),
+		Almacen 		= require('../models/almacen'),
+		Producto 		= require('../models/producto');
 
 
 
@@ -11,13 +13,34 @@ router.use(bodyParser.urlencoded({extende: true}));
 
 router.use(methodOverride('_method'));
 
-var almacen = "";
+var almacen = [];
+
+var producto = "";
 
 router.get('/articulo/new', function(req,res) {
+	// almacen = 
 
-}
+	Almacen.find({}, function(err,found) {
+		if(!err) {
+			// console.log(found[0].nombre);
+			almacen = found;
+		} else {
+			console.log(err);
+		}
+	});
 
-);
+	
+
+	Producto.find({},function(err,found){
+		if(!err) {
+			console.log(almacen[0].nombre);
+			res.render('nuevoArticulo',{almacen: almacen, producto: found});
+		} else {
+			console.log(err);
+		}
+	});
+
+} );
 
 router.get('/articulo', (req,res) => Articulo.find({}, (err,found) => 
 
