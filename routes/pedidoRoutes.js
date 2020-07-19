5781c92605df62b5570f88f5d29ae2877fe8bcb8
Raudo Cespedes <raj.cespedes@gmail.com>
2020-07-18 23:48:47 -0400

    Adding pedidos routes

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -1,11 +1,20 @@
 const express 	= require('express'),
 		router	= express.Router(),
-		Pedido 	= require('../models/pedido');
+		Pedido 	= require('../models/pedido'),
+		Articulo = require('../models/articulo');
 
 
 router.get('/pedido', (req,res) => 
 
 	Pedido.find({}).populate('articulo').exec( (err,found) => !err ? res.render('pedidoIndex',{ pedido: found }) : console.log(err) ) 
+
+);
+
+router.get('/pedido/new',(req,res) => 
+
+	Articulo.find({}).populate('producto').populate('almacen').exec( (err,found) => 
+
+		!err ? res.render('nuevoPedido', { articulo: found}) : console.log(err) )  
 
 );
 
