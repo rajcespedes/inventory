09d78b6ae65ae0924cdc9ac7e0292e8df4c2ccd2
Raudo Cespedes <raj.cespedes@gmail.com>
2020-09-30 23:12:49 -0400

    Finding a way to capture quantities

--- public/jQueryFile.js
+++ public/jQueryFile.js
@@ -21,10 +21,10 @@ function jQueryFile() {
 		if($(this).is(':checked')){
 			dataHolder.push($(this).val());
 			precioHolder.push($(`#q${$(this).val()}`).text());
-			
+			cantidadHolder.push(($(`#p${$(this).val()}`).val()));
 			// $(`#p${$(this).val()}`).change( () => console.log($(`#p${$(this).val()}`).val()));
 			
-			// console.log(dataHolder);
+			// console.log(dataHolder, precioHolder, cantidadHolder);
 		}
 	});
 	
@@ -32,37 +32,16 @@ function jQueryFile() {
 
 		function (){
 
-			// alert('worked');
-
-			// $('input[type=checkbox]').click( function() {
-				// if($('input[type=checkbox]').is(':checked')){
-					if($('input[name=pedido]:checked')){
-					// alert($(`#p${$('input[name=pedido]:checked').val()}`).val());
-					cantidadHolder.push($(`#p${$('input[name=pedido]:checked').val()}`).val());
+			$('input[type=checkbox]').click( function() {
+				if($(this).is(':checked')){
+					// dataHolder.push($(this).val());
+					// precioHolder.push($(`#q${$(this).val()}`).text());
+					cantidadHolder.push(($(`#p${$(this).val()}`).val()));
+					// $(`#p${$(this).val()}`).change( () => console.log($(`#p${$(this).val()}`).val()));
+					
 					console.log(cantidadHolder);
 				}
-			// });
-		
-
-			// var trying = $('input[name=pedido]:checked').map( function () {
-			// 	return $(this).val();
-			// }).get();
-			
-			// if($('input[type=checkbox]'))
- 
-			// var attempt = $('td').children(`input[type=number]`).map( function () {
-			// 	return $(this).val();
-			// }).get();		
-
-			// var take = attempt.filter( e => e > 0);
-
-			// var holder = [];
-
-			// trying.forEach( function(element) {
-			// 	holder.push( $(`#q${element}`).text()); 
-			// } );
-
-			// var totalHolder = [];
+			})
 
 			$.post('http://localhost:3000/pedido/',{dataHolder},function(data,status){});		
 
