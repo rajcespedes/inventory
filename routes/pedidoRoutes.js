fbeafd6ac3b42aa445c4c3d91ca4ecd283848d76
Raudo Cespedes <raj.cespedes@gmail.com>
2020-10-04 22:20:28 -0400

    Working around the purchase process

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -25,50 +25,16 @@ var precioHolder = [];
 
 var actualDate = new Date();
 
-router.post('/pedido', function(req,res) {
+// router.post('/pedido', function(req,res) {
 
-	// console.log(precioHolder);
-
-	// Pedido.create({
-	// 	cantidad: req.body.take,
-	// 	fecha: actualDate.toLocaleDateString(),
-	//  articulo: 
-
-
-	// }, function(err,crated){});
-	
-	// req.body.pedido.cantidad.forEach( function(value){
-	// 	if (value != "") {
-	// 		cantidadHolder.push(value);
-			
-	// 	}
-	// } );
-
-	// console.log("actual value " + cantidadHolder);
-
-	// Articulo.create({
-	// 	cantidad: req.body.pedido.
-	// }, function(err,added){});
-
-	// req.body.sendToBack.forEach( element => 
-
-	// 	Producto.find({descripcion: element },'_id').exec( function(err,res) { 
-
-	// 		articuloHolder.push(res);
-
-	// 		articuloHolder.forEach( id => id.forEach( innerId => console.log(innerId._id)));
-
-	// 	} )
-
-	// console.log
-	//  );
-
-	// console.log(req.body.toCut);
-
-	req.body.toCut.forEach(i => console.log('element ', i.forEach( t => t)));
+// 	res.redirect();
 
 	
 
+// });
+
+router.get('/pedidos/cantidad', function(req,res){
+	res.render('cantidad');
 });
  
 
