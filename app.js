cb648b10cf480ffffdcc205fde25a2ebc4da5014
Raudo Cespedes <raj.cespedes@gmail.com>
2020-10-16 00:38:19 -0400

    Sending data to the reporte route

--- app.js
+++ app.js
@@ -39,57 +39,25 @@ app.use(pedidoRoutes);
 // Pedido.remove({},(err,res) => console.log('removed'));
 
 var convert = []; 
-// = {
-// 		"Product": "Apple",
-// 		"Price": 2.50
-// 	};
 
 app.get('/report', function(req,res) {
-	// Pedido.find({}).populate('articulo').exec( function(err,data) {
-	// 	if(!err){
-	// 		console.log(convert);
-			
-	// 		res.render('report', { 
-	// 			data: convert
-	// 			// JSON.stringify(data) 
-	// 		});
-			
-	// 	} else {
-	// 		console.log(err);			
-	// 	}
-	// 	});
 
 	Pedido.find({}, function (err,data)  {
 		
 		if(data) {
-			data.forEach( info => convert.push(
-				{
-					cantidad: info['cantidad']
-				}
+			// data.forEach( info => convert.push(
+			// 	{
+			// 		cantidad: info['cantidad']
+			// 	}
 
-			) );
-			// convert = [{
-			// 	cantidad: data[0]['cantidad'],
-			// 	fecha: data[0]['fecha']
-			// }];
-			console.log(convert);
-			res.render('report', {dataSent: convert});
+			// ) );
+
+			console.log('this is the data received ', data);
+			res.render('report', {dataSent: data});
 		} else {
 			console.log(err);
 		} 
 	
-	// populate('articulo').exec( function(data,err)
-	// {
-	// 	if(data) {
-	// 		res.render('report',{dataFound: data});
-	// 	} else {
-	// 		console.log(err);
-	// 	}
-	// });
-	// // console.log(data);
-	// res.render('report');
-	
-
 })
 });
 
